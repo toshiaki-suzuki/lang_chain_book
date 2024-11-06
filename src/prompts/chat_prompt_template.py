6d3b73ea4f05780c56401ff42f46f62df42c094e
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-11-06 21:04:55 +0900

    chat template のサンプルにLLMへのリクエストを追加

--- src/prompts/chat_prompt_template.py
+++ src/prompts/chat_prompt_template.py
@@ -1,4 +1,5 @@
 import os
+from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, PromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
 
@@ -11,13 +12,19 @@ template = """
 """
 
 def main():
+  chat = ChatOpenAI(
+    model_name="gpt-3.5-turbo-0125",
+    temperature=0,
+    api_key=api_key)
+
   chat_prompt = ChatPromptTemplate.from_messages([
     SystemMessagePromptTemplate.from_template(" あなたは{country}料理のプロフェッショナルです。"),
     HumanMessagePromptTemplate.from_template(" 以下の料理のレシピを考えてください。 \n\n料理名: {dish} "),
   ])
 
   messages = chat_prompt.format_prompt(country="中華", dish="麻婆豆腐").to_messages()
-  print(messages)
+  result = chat.invoke(messages)
+  print(result.content)
 
 if __name__ == '__main__':
     main()
